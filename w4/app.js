1058e244d717deb1f1f66a77820789a14db12817
Dominic Taing <0952773@my.scccd.edu>
2025-08-30 14:31:09 -0700

    fix: updated code after reviewed: updated code 3.1 for eventlistener setup

--- w4/app.js
+++ w4/app.js
@@ -1,1 +1,12 @@
 console.log('Hello from app.js! Your JavaScript is connected and running!');
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("greetingForm");
+  const nameInput = document.getElementById("nameInput");
+  const greetingOutput = document.getElementById("greetingOutput");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    
+
